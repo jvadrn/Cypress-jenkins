fed6351a3a6fbafaf5945cb93804260b13cd7b7b
Jevi Adriansyah <jeviadriansyah25@gmail.com>
2024-06-13 11:34:51 +0700

    udate egen

---
+++ cypress/e2e/1-getting-started/testOne.cy.js
@@ -0,0 +1,18 @@
+
+describe('Login', ()=>{
+  
+  it('Login', () => {
+    cy.visit('http://localhost:3000/Index.html')
+    // cy.get('Username').type('admin_portal');
+    // cy.get('input[ng-reflect-name="password"]').type('P@ssw0rd');
+    // cy.get('.btn').click() 
+    // cy.get('input[ng-reflect-name="username"]').type('p@gmail.com');
+    // cy.get('.btn').click() 
+    // cy.get('input[ng-reflect-name="username"]').type('admin_portal');
+    // cy.get('input[ng-reflect-name="password"]').type('P@ssw0rd');
+    // cy.get('.btn').click()
+    
+    
+  })
+  
+})
