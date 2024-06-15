994b2e02ad1f6b30d00e2fc1f8ddc38ed64b40aa
Jevi Adriansyah <jeviadriansyah25@gmail.com>
2024-06-16 02:43:21 +0700

    update

--- cypress/e2e/1-getting-started/testOne.cy.js
+++ cypress/e2e/1-getting-started/testOne.cy.js
@@ -1,11 +1,7 @@
 describe('test-one', ()=>{
   
   it('Login', () => {
-    cy.visit('http://localhost:3000/index.html')
-    cy.get('input[name="name"]').type('TestQAone');
-    cy.get('input[name="email"]').type('p@gmail.com');
-    cy.get('input[name="nik"]').type('1122282383782893');
-    cy.get('input[name="phone"]').type('28238378278');
-    cy.get('button').click();
+    cy.visit('https://nginx.org/en/download.html')
+    
   })  
 })
