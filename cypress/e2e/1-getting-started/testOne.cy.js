59475158ca61fa2adcb482e20d2119423db28aa7
Jevi Adriansyah <jeviadriansyah25@gmail.com>
2024-06-16 05:42:44 +0700

    update

--- cypress/e2e/1-getting-started/testOne.cy.js
+++ cypress/e2e/1-getting-started/testOne.cy.js
@@ -1,11 +1,11 @@
 describe('test-one', ()=>{
   
   it('Login', () => {
-    cy.visit('http://localhost:4200/')
-    cy.get('input[name="name"]').type('TestQAone');
-    cy.get('input[name="email"]').type('p@gmail.com');
-    cy.get('input[name="nik"]').type('1122282383782893');
-    cy.get('input[name="phone"]').type('28238378278');
-    cy.get('button').click();
+    cy.visit('https://demoqa.com/automation-practice-form')
+    // cy.get('input[name="name"]').type('TestQAone');
+    // cy.get('input[name="email"]').type('p@gmail.com');
+    // cy.get('input[name="nik"]').type('1122282383782893');
+    // cy.get('input[name="phone"]').type('28238378278');
+    // cy.get('button').click();
   })  
 })
