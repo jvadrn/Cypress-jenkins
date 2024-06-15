a6d81c988f4fb6b86cdb7131f33cff2312d6a3d5
Jevi Adriansyah <jeviadriansyah25@gmail.com>
2024-06-16 05:50:46 +0700

    update

--- cypress/e2e/1-getting-started/testOne.cy.js
+++ cypress/e2e/1-getting-started/testOne.cy.js
@@ -1,7 +1,7 @@
 describe('test-one', ()=>{
   
   it('Login', () => {
-    cy.visit('https://demoqa.com/automation-practice-form')
+    cy.visit('https://angular.dev/')
     // cy.get('input[name="name"]').type('TestQAone');
     // cy.get('input[name="email"]').type('p@gmail.com');
     // cy.get('input[name="nik"]').type('1122282383782893');
