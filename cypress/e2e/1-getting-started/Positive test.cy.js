585a890e56874e12615e3c464d26fbc40e0281b4
Jevi Adriansyah <jeviadriansyah25@gmail.com>
2024-06-19 14:07:05 +0700

    update

--- cypress/e2e/1-getting-started/Positive test.cy.js
+++ cypress/e2e/1-getting-started/Positive test.cy.js
@@ -5,7 +5,7 @@ describe('Positif', ()=>{
     cy.get('input[name="name"]').type('TestQAone');
     cy.get('input[name="email"]').type('p@gmail.com');
     cy.get('input[name="nik"]').type('1122282383782893');
-    cy.get('input[name="phone"]').type('28238378278');
+    cy.get('input[name="phone"]').type('2823ee8378278');
     cy.get('button').click();
   })  
 })
